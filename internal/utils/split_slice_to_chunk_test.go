9a7bd0f46db900b72d6a8638201544e0b2d86e89
Kamil Masalimov <masalimov.ka@gmail.com>
2021-08-06 18:31:37 +0500

    Add function for split slice to chunks

---
+++ internal/utils/split_slice_to_chunk_test.go
@@ -0,0 +1,51 @@
+package utils
+
+import (
+	"github.com/stretchr/testify/assert"
+	"testing"
+)
+
+func TestSplitSliceToChunk(t *testing.T) {
+	var testTable = []struct {
+		slice     []int
+		chunkSize int
+		result    [][]int
+	}{
+		{
+			slice:     []int{1, 2},
+			chunkSize: 1,
+			result:    [][]int{[]int{1}, []int{2}},
+		},
+		{
+			slice:     []int{1, 2, 3, 4, 5},
+			chunkSize: 2,
+			result:    [][]int{[]int{1, 2}, []int{3, 4}, []int{5}},
+		},
+		{
+			slice:     nil,
+			chunkSize: 2,
+			result:    nil,
+		},
+		{
+			slice:     []int{1, 2, 3, 4, 5},
+			chunkSize: -1,
+			result:    nil,
+		},
+		{
+			slice:     []int{},
+			chunkSize: 1,
+			result:    [][]int{},
+		},
+		{
+			slice:     []int{1, 2},
+			chunkSize: 10,
+			result:    [][]int{[]int{1, 2}},
+		},
+	}
+
+	for _, testCase := range testTable {
+		result, _ := SplitSliceToChunk(testCase.slice, testCase.chunkSize)
+
+		assert.Equal(t, testCase.result, result)
+	}
+}
